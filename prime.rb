26deeaf54ad812eab770f506d221943c946faf59
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-27 17:49:32 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(int)
-  for num in 2..int do
-    if int % num == 0 
-      false
-    end
-  end
-  true
+  # for num in 2..int do
+  #   if int % num == 0 
+  #     false
+  #   end
+  # end
+  # true
 end
